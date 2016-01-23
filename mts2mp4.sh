fb0effeb39cb8ffed69fb2dbe638566a0fda11fb
Zac610 <ramas_rtl@hotmail.com>
2016-01-23 18:53:43 +0100

    Create mts2mp4.sh
    First version of the script

---
+++ mts2mp4.sh
@@ -0,0 +1,83 @@
+#/bin/sh
+
+#~ set -x
+
+IFS=$(echo -en "\n\b")
+
+mtsDir="/media/ZAC 64 REC/HBPVR"
+
+mtsFile=$1
+filmName=$2
+startTime=$3
+durationTime=$4
+
+if [[ -z $1 ]]; then
+	read -p "Inserisci il nome del file .mts: " mtsFile
+	if [[ -z $mtsFile ]]; then
+		exit
+	fi
+fi
+if [[ -z $2 ]]; then
+	read -p "Inserisci il nome del film [film]: " filmName
+	if [[ -z $filmName ]]; then
+		filmName="film"
+	fi
+fi
+if [[ -z $3 ]]; then
+	read -p "Inserisci il tempo di partenza: " startTime
+fi
+if [[ -z $4 ]]; then
+	read -p "Inserisci la durata: " durationTime
+fi
+
+if  [[ -n $startTime ]]; then
+	startTime="-ss $startTime"
+fi
+
+if  [[ -n $durationTime ]]; then
+	durationTime="-t $durationTime"
+fi
+
+echo $mtsFile
+echo $filmName
+echo $startTime
+echo $durationTime
+
+#~ exit
+
+rm command
+
+for file in $(ls -1 "$mtsDir"/$mtsFile* | sort -V); do
+	echo \""$file"\">>command
+done
+
+echo "cat $(tr '\n' ' '<command)> output.ts" > command1
+
+. command1
+
+rm command
+rm command1
+
+time avconv -i output.ts $startTime $durationTime -c:a copy -c:v libx264 -crf 18 -preset veryfast $filmName.mp4
+
+exit
+
+outputFile='outputfile.mp4'
+
+rm -f $outputFile
+
+command='avconv -i "concat:'
+
+for file in $(ls -1X *.mts*); do
+	command=${command}$file"|"
+done
+
+command=${command}"|"
+
+command=${command/||/\"}
+
+command=${command}" -c:v libx264 -strict experimental "$outputFile
+
+eval $command
+
+# avconv -i input.ts -c:a copy -c:v libx264 -crf 18 -preset veryfast output.mp4
